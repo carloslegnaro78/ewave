19c06f812385a192d3d84c967a2f5b560d844714
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-08-30 23:41:12 -0300

    Crud -  Pedido

--- ewave/app/Http/Controllers/Api/PedidoController.php
+++ ewave/app/Http/Controllers/Api/PedidoController.php
@@ -9,34 +9,94 @@ use App\Models\Pedido;
 class PedidoController extends Controller
 {
 
-    public function index( Pedido $pedido)
+    private $pedido;
+
+    public function __construct(Pedido $pedido)
     {
-        
+
+        $this->pedido = $pedido;
+    }
+
+    public function index(Pedido $pedido)
+    {
+
         $pedido = $pedido->get();
 
         return response()->json($pedido);
-
     }
 
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'cliente_id' =>    'required',
+            'pastel_id' =>   'required'
+        ]);
+
+        $pedido = Pedido::create([
+            'cliente_id' => $request->input('cliente_id'),
+            'pastel_id' => $request->input('pastel_id')
+        ]);
+
+        return response()->json($pedido, 201);
     }
 
 
     public function show($id)
     {
-        //
+        if (!$pedido = $this->pedido->find($id))
+            return response()->json(['error' => 'Not found'], 404);
+
+        return response()->json($pedido);
     }
 
-  
+
 
     public function update(Request $request, $id)
     {
-        //
+        $pedido = $this->pedido->find($id);
+
+        if (!$pedido)
+            return response()->json(['error' => 'Not found'], 404);
+
+        $pedido->update($request->all());
+
+        return response()->json($pedido);
     }
 
+
+    public function softDeleted()
+    {
+        $pedido = Pedido::onlyTrashed()->get();
+
+        $response = $this->successfulMessage(200, 'Successfully', true, $pedido->count(), $pedido);
+        return response($response);
+    }
+
+    public function restore($id)
+    {
+
+        $pedido = Pedido::onlyTrashed()->find($id);
+
+        if (!is_null($pedido)) {
+
+            $pedido->restore();
+            $response = $this->successfulMessage(200, 'Successfully restored', true, $pedido->count(), $pedido);
+        }
+        return response($response);
+    }
+
+    public function permanentDeleteSoftDeleted($id)
+    {
+        $pedido = Pedido::onlyTrashed()->find($id);
+
+        if (!is_null($pedido)) {
+
+            $pedido->forceDelete();
+            $response = $this->successfulMessage(200, 'Successfully deleted', true, 0, $pedido);
+        }
+        return response($response);
+    }
 
     public function destroy($id)
     {
